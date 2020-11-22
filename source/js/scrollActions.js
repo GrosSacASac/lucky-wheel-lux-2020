a0db00b8b75f16acedb8f2460608f87d7e7fe617
Cyril Walle <cyril.walle@protonmail.com>
2020-11-22 09:22:45 +0100

    fix

--- source/js/scrollActions.js
+++ source/js/scrollActions.js
@@ -5,45 +5,45 @@ import { MOVE_VIRTUALLY } from "./eventNames.js";
 
 
 const start = function (eventEmitter) {
-    const intersectionObserver = new IntersectionObserver((observedEntries) => {
-        observedEntries.forEach(observedEntry => {
-            if (observedEntry.intersectionRatio > 0) {
-                console.log(observedEntry.target)
-                if (observedEntry.target === d.elements[`end`]) {
-                    observedEntry.target.insertAdjacentHTML("beforebegin", "<h2 data-element>LOL</h2>")
-                } else {
-                    observedEntry.target.insertAdjacentHTML("afterend", "<h2 data-element>LOL</h2>")
-                }
-                console.log("is visible")
-            } else {
-                console.log(observedEntry.target)
-                console.log("not visible")
+    // const intersectionObserver = new IntersectionObserver((observedEntries) => {
+    //     observedEntries.forEach(observedEntry => {
+    //         if (observedEntry.intersectionRatio > 0) {
+    //             console.log(observedEntry.target)
+    //             if (observedEntry.target === d.elements[`end`]) {
+    //                 observedEntry.target.insertAdjacentHTML("beforebegin", "<h2 data-element>LOL</h2>")
+    //             } else {
+    //                 observedEntry.target.insertAdjacentHTML("afterend", "<h2 data-element>LOL</h2>")
+    //             }
+    //             console.log("is visible")
+    //         } else {
+    //             console.log(observedEntry.target)
+    //             console.log("not visible")
 
-            }
-        })
-    }, {
-        threshold: [0,1]
-    });
+    //         }
+    //     })
+    // }, {
+    //     threshold: [0,1]
+    // });
 
     // [d.elements[`begin`], d.elements[`end`]].forEach((element) => {
     //     intersectionObserver.observe(element);
     // });
 
-    // const intersectionObserver2 = new IntersectionObserver((observedEntries) => {
-    //     observedEntries.forEach(observedEntry => {
-    //         if (observedEntry.intersectionRatio >= 1) {
-    //             // console.log("is largely visible")
-    //             eventEmitter.emit(MOVE_VIRTUALLY, {
-    //                 element: observedEntry.target,
-    //             })
-    //         } else if (observedEntry.intersectionRatio <= 0.7){
-    //             // console.log("not so visible")
+    const intersectionObserver2 = new IntersectionObserver((observedEntries) => {
+        observedEntries.forEach(observedEntry => {
+            if (observedEntry.intersectionRatio >= 1) {
+                // console.log("is largely visible")
+                eventEmitter.emit(MOVE_VIRTUALLY, {
+                    element: observedEntry.target,
+                })
+            } else if (observedEntry.intersectionRatio <= 0.7){
+                // console.log("not so visible")
 
-    //         }
-    //     })
-    // }, {
-    //     threshold: [1,0.7]
-    // });
+            }
+        })
+    }, {
+        threshold: [1,0.7]
+    });
 
     Array.from(document.getElementsByClassName("object")).forEach((element) => {
         intersectionObserver2.observe(element);
